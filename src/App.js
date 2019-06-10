0a06100b897b894e4da2aae083ef2e46f260a6af
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-06-10 20:07:09 +0200

    Added the Title component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import React from 'react';
+import {Component} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
 
-function App()
-{
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to Codaisseur</h2>
-        <h2>Welcome to Codaisseur</h2>
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Codaisseur</h2>
+          <p>Hello World!</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <main>
+          <Title content="Some Simple Title" />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
