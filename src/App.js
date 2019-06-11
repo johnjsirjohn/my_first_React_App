22f43537bab920e7729e143ee98602c37451f30b
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-06-11 11:53:24 +0200

    Added a LightSwitch file and button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title';
+import LightSwitch from './components/LightSwitch';
 
 // function App() {
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
           <h2>Welcome to Codaisseur</h2>
           <p>Hello World!</p>
           <a
@@ -25,6 +27,7 @@ class App extends Component {
         </header>
         <main>
           <Title content="Some Simple Title" />
+          <LightSwitch />
         </main>
       </div>
     );
